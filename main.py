d1d30ef94aff2d8cc8520e8a85eff3c4e76c597c
hwanhee0728 <155794791+hwanhee0728@users.noreply.github.com>
2024-11-26 17:15:29 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+#__import__('pysqlite3')
+#import sys
+#sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 import os
 os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
@@ -18,28 +18,27 @@ from PIL import Image
 load_dotenv()
 password_key = os.getenv('KEY')
 
-st.markdown('<a href="https://surveykmla.streamlit.app/" target="_blank">챗봇 사용후 Feedback주실 분 여기 클릭!</a>', unsafe_allow_html=True)
-image = Image.open('chatbot21.png')
-image = image.resize((600, 56))
+image = Image.open('robot01.png')
+image = image.resize((150, 150))
 st.image(image)
 
-password = st.text_input(":strawberry: 7글자 패스워드를 넣어주세요!", type="password")
+st.header("KMLA Chatbot")
+
+password = st.text_input(":name_badge: 7글자 패스워드를 넣어주세요!", type="password")
 
 if password:
     if password == password_key:
 
-        st.success("패스워드 확인 완료!")
         st.write("")
-        user_input = st.text_input(":apple: 민사고에 대해 질문하고 엔터를 눌러주세요!")
+        user_input = st.text_input(":eight_pointed_black_star:민사고에 대해 질문하고 엔터를 눌러주세요!")
 
         if user_input:
             # 질문하면 아래 내용 보여주기
-            st.write("")
-            st.write(':tangerine::tangerine::tangerine::tangerine: 답변 드립니다 :tangerine::tangerine::tangerine::tangerine:')
+            st.write(':pencil2::pencil2::pencil2: 답변 드립니다 :pencil2::pencil2::pencil2:')
 
             embeddings_model = OpenAIEmbeddings(model="text-embedding-ada-002")
             try:
-                db = Chroma(persist_directory="chromadb_ada", embedding_function=embeddings_model)
+                db = Chroma(persist_directory="chromadb_ada2", embedding_function=embeddings_model)
             except Exception as e:
                 st.error(f"Error initializing database: {e}")
                 raise
@@ -57,7 +56,7 @@ if password:
             question = user_input
             chat_box = st.empty()
             stream_handler = StreamHandler(chat_box)
-            llm = ChatOpenAI(model_name="gpt-4o", temperature=0, streaming=True, callbacks=[stream_handler], max_tokens=2000)
+            llm = ChatOpenAI(model_name="gpt-4o", temperature=0.2, streaming=True, callbacks=[stream_handler])
             qa_chain = RetrievalQA.from_chain_type(llm, retriever=db.as_retriever())
 
             try:
