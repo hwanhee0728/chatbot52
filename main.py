de4e5c735430db88078b94ffbfd697b93b844b87
hwanhee0728 <155794791+hwanhee0728@users.noreply.github.com>
2024-11-26 17:16:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-#__import__('pysqlite3')
-#import sys
-#sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 import os
 os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
