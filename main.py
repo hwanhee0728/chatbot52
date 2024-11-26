158c1fbbe775ff942f7c783dd2649dbf0fd57f57
hwanhee0728 <155794791+hwanhee0728@users.noreply.github.com>
2024-11-26 18:34:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
 
 import streamlit as st
 
-from langchain_community.vectorstores import Chroma
 from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
 from langchain_openai import ChatOpenAI
