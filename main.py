731d0d02fa65a8440b80fa7efe881a675a5e3159
hwanhee0728 <155794791+hwanhee0728@users.noreply.github.com>
2024-11-26 18:09:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ password = st.text_input(":name_badge: 7글자 패스워드를 넣어주세요!"
 
 if password:
     if password == password_key:
+        st.success("패스워드 확인 완료!")
         user_input = st.text_input(":eight_pointed_black_star:민사고에 대해 질문하고 엔터를 눌러주세요!")
 
         if user_input:
